83d2781729894443053bf0add94cc1b737f4ca6e
Gregory Ashton <gregory.ashton@ligo.org>
2020-06-02 11:42:21 +1000

    Clean up fake data

--- example/make_fake_data.py
+++ example/make_fake_data.py
@@ -7,10 +7,10 @@ import frodo
 
 # Injection parameters
 pulse_injection_parameters = dict(
-    C0=0.8, C1=0.1, C2=0.3, beta=0.003, toa=0.05, base_flux=0.1)
+    C0=0.8, C1=0.1, C2=0.3, C3=0.01, beta=0.003, toa=0.05, base_flux=0.0)
 
-# Instantiate a flux model with 3 components
-flux_model = frodo.flux_model.SinglePulseFluxModel(3)
+# Instantiate a flux model with 4 components
+flux_model = frodo.flux_model.SinglePulseFluxModel(4)
 
 # Generate fake data using the instantiated flux model and injection parameters
 N = 1000
@@ -21,3 +21,7 @@ pulse_number = 0
 # Write the data to a text file
 df = pd.DataFrame(dict(time=time, flux=flux, pulse_number=pulse_number))
 df.to_csv("fake_data.txt", index=False)
+
+fig, ax = plt.subplots()
+ax.plot(time, flux)
+fig.savefig("fake_data.png")
